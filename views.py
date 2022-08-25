ab2c73895813fb7c281a364b854be5e7e4f1582e
三儿 <2367183861@qq.com>
2022-08-25 07:52:54 -0700

    create view login

--- views.py
+++ views.py
@@ -1,5 +1,8 @@
 from django.http inport HttpResponse
-
+from django.shortcuts import redirects
 
 def index(request):
 	return HttpResponse('index')
+
+def login(request):
+	return redirect("/index")
